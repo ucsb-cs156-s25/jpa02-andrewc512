5cb2d3c2cd7ec9b8399189113c1ec38801f6a523
Andrew Chen <andrew_chen@ucsb.edu>
2025-04-08 21:14:28 -0700

    ac -completed mutation coverage for hashCode

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -2,6 +2,9 @@ package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.beans.Transient;
+import java.util.Arrays;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -19,8 +22,64 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
-   
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
 
+    @Test
+    public void equals_same_object() {
+        assert(team.equals(team));
+    }
+
+    @Test
+    public void equals_different_object() {
+        String notATeam = "Not a team";
+        assert(!team.equals(notATeam));
+    }
+
+    @Test
+    public void equals_same_fields(){
+        Team team2 = new Team("test-team");
+        assert(team.equals(team2));
+    }
+
+    @Test
+    public void equals_diff_names(){
+        Team team2 = new Team("test-team-2");
+        assert(!team.equals(team2));
+    }
+
+    @Test
+    public void hashCode_same_name(){
+        Team team2 = new Team("test-team");
+        assert(team.hashCode() == team2.hashCode());
+    }
+
+    @Test
+    public void hashCode_diff_name(){
+        Team team2 = new Team("test-team-2");
+        assert(team.hashCode() != team2.hashCode());
+    }
+
+    @Test
+    public void hashCode_diff_mems(){
+        Team team2 = new Team("test-team");
+        team2.addMember("Gaucho");
+        assert(team.hashCode() != team2.hashCode());
+    }
+
+    @Test
+    public void hashCode_diff_names_and_mems(){
+        Team team2 = new Team("test-team-2");
+        team2.addMember("Gaucho");
+        assert(team.hashCode() != team2.hashCode());
+    }
+
+    @Test
+    public void hashCode_replace_and(){
+        int hash1 = team.hashCode();
+        int hash2 = team.getName().hashCode() & team.getMembers().hashCode();
+        assert(hash1 != hash2);
+    }
 }
