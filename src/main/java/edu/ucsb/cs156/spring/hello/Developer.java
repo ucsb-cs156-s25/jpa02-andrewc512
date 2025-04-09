f291b3039437720c6d0d2fee3cbff44bf7dc9034
Andrew Chen <andrew_chen@ucsb.edu>
2025-04-08 17:56:55 -0700

    ac - added tests for team name and members

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -34,14 +34,13 @@ public class Developer {
      */
     
     public static Team getTeam() {
-        // TODO: Change this to your team name
-        Team team = new Team("s25-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("s25-10");
+        team.addMember("Andrew C");
+        team.addMember("Benjamin S");
+        team.addMember("Junjie L");
+        team.addMember("Matthew Z");
+        team.addMember("Mihir K");
+        team.addMember("Milad H");
         return team;
     }
 }
--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -3,6 +3,7 @@ package edu.ucsb.cs156.spring.hello;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.beans.Transient;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Modifier;
 
@@ -31,5 +32,23 @@ public class DeveloperTest {
     public void getGithubId_returns_correct_githubId(){
         assertEquals("andrewc512", Developer.getGithubId());
     }
+
+    @Test
+    public void getTeam_returns_team_with_correct_name(){
+        Team t = Developer.getTeam();
+        assertEquals("s25-10", t.getName());
+    }
+
+    @Test
+    public void getTeam_returns_team_with_correct_members() {
+        Team  t = Developer.getTeam();
+        assertTrue(t.getMembers().contains("Andrew C"),"Team should contain Andrew C");
+        assertTrue(t.getMembers().contains("Benjamin S"),"Team should contain Benjamin S");
+        assertTrue(t.getMembers().contains("Junjie L"),"Team should contain Junjie L");
+        assertTrue(t.getMembers().contains("Matthew Z"),"Team should contain Matthew Z");
+        assertTrue(t.getMembers().contains("Mihir K"),"Team should contain Mihir K");
+        assertTrue(t.getMembers().contains("Milad H"),"Team should contain Milad H");
+    }
+
 
 }
